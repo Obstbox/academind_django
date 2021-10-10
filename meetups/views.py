7e74ecb743dd17eab1f5fa06bbca6d0e78bb939c
ivan <ivan pashchenko>
2021-10-10 23:01:37 +0500

    01:03:56> more complex yet creepy markup

--- meetups/views.py
+++ meetups/views.py
@@ -4,8 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     meetups = [
-        {'title': 'A First Meetup'},
-        {'title': 'A Secont Meetup'},
+        {
+            'title': 'A First Meetup', 
+            'location': 'New York', 
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A Secont Meetup', 
+            'location': 'Paris', 
+            'slug': 'a-second-meetup'
+        }
     ]
     return render(request, 'meetups/index.html', {
         'show_meetups': True,
